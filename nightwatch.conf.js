782f2b5ac99279e59aef232bcdda2cb360685ff8
Chris Mills <cmills@breakfreesolutions.com>
2021-11-05 16:51:58 -0500

    Updated nightwatch.conf.js and added new delete-on-leave test suite
    Due to Response 500 errors when running multiple test suites, the nightwatch
    configuration file was updated to included parallelized workers. Due to running
    multiple workers (i.e. threads) and therefore sessions, this resolves the 500
    error as there is a dedicated server instance for each test suite (i.e. separate
    file in `tests` directory).
    
    Using the original `integration.js` test suite, a new `delete-on-leave.js` test
    suite was added to the `tests` directory. This includes a simple test to confirm
    that existing whiteboard elements are removed from the board on refresh.

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -5,13 +5,18 @@ const Services = {}; loadServices();
 module.exports = {
   "src_folders": ["tests"],
 
+  "test_workers": {
+    "enabled": true,
+    "workers": "auto"
+  },
+
   "webdriver": {
     "start_process": true,
     "server_path": "./node_modules/.bin/geckodriver",
     "cli_args": [
       "--log", "debug"
     ],
-    "port": 4444
+    "port": 4444,
   },
 
   "test_settings": {
---
+++ tests/delete-on-leave.js
@@ -0,0 +1,58 @@
+const fs = require("../server/fs_promises.js");
+const os = require("os");
+const path = require("path");
+
+const PORT = "8488"
+const SERVER = 'http://localhost:' + PORT;
+
+let wbo_delete, data_path;
+
+async function beforeEach(browser, done) {
+    data_path = await fs.promises.mkdtemp(path.join(os.tmpdir(), 'wbo-test-data-'));
+    process.env["PORT"] = PORT;
+    process.env["WBO_HISTORY_DIR"] = data_path;
+    console.log("Launching WBO in " + data_path);
+    wbo_delete = require("../server/server.js");
+    done();
+}
+
+async function afterEach(browser, done) {
+    wbo_delete.close();
+    done();
+}
+
+function testDeleteOnRefreshWithOneUser(browser) {
+    return browser
+        .assert.titleContains('WBO')
+        .click('.tool[title ~= Pencil]') // pencil
+        .assert.cssClassPresent('.tool[title ~= Pencil]', ['curTool'])
+        .executeAsync(async function (done) {
+            function sleep(t) {
+                return new Promise(function (accept) { setTimeout(accept, t); });
+            }
+            Tools.setColor('#123456');
+            Tools.curTool.listeners.press(100, 200, new Event("mousedown"));
+            await sleep(80);
+            Tools.curTool.listeners.release(300, 400, new Event("mouseup"));
+            done();
+        })
+        .assert.elementPresent("path[d='M 100 200 L 100 200 C 100 200 300 400 300 400'][stroke='#123456']")
+        .refresh()
+        .assert.not.elementPresent("path[d='M 100 200 L 100 200 C 100 200 300 400 300 400'][stroke='#123456']")
+        .back()
+}
+
+
+function testBoardDelete(browser) {
+    var page = browser.url(SERVER + '/boards/delete-on-leave')
+        .waitForElementVisible('.tool[title ~= Pencil]') // pencil
+    page = testDeleteOnRefreshWithOneUser(page);
+
+    // test hideMenu
+    browser.url(SERVER + '/boards/delete-on-leave?hideMenu=true').waitForElementNotVisible('#menu');
+    browser.url(SERVER + '/boards/delete-on-leave?hideMenu=false').waitForElementVisible('#menu');
+
+    page.end();
+}
+
+module.exports = { beforeEach, testBoardDelete, afterEach };
