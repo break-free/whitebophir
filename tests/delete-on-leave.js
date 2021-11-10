778d0053d55e3aca1063cab84d49b5351d66ddc0
Chris Mills <cmills@breakfreesolutions.com>
2021-11-10 17:56:06 -0600

    Board deletion does not occur for two users test
    Once the board is open in one window, another window is opened. The
    element is checked to make sure it still exists, even after the window
    is refreshed. The second window is closed, and elements confirmed to
    still exist in the first window. The first window is refreshed, ending
    the board session, and then the element confirmed as removed.
    
    This test confirms that the board is not deleted/wiped until there are
    all users have left.

--- tests/delete-on-leave.js
+++ tests/delete-on-leave.js
@@ -2,8 +2,9 @@ const fs = require("../server/fs_promises.js");
 const os = require("os");
 const path = require("path");
 
-const PORT = "8488"
+const PORT = "8488";
 const SERVER = 'http://localhost:' + PORT;
+const URL = SERVER + '/boards/delete-on-leave';
 
 let wbo_delete, data_path;
 
@@ -42,16 +43,53 @@ function testDeleteOnRefreshWithOneUser(browser) {
         .back()
 }
 
+// TODO: Dummy function that copies the same test as testDeleteOnRefreshWithOneUser.
+
+function testDeleteOnRefreshWithTwoUsers(browser) {
+    return browser
+        .assert.titleContains('WBO')
+        .click('.tool[title ~= Pencil]') // pencil
+        .assert.cssClassPresent('.tool[title ~= Pencil]', ['curTool'])
+        .executeAsync(async function (done) {
+            function sleep(t) {
+                return new Promise(function (accept) { setTimeout(accept, t); });
+            }
+            Tools.setColor('#123456');
+            Tools.curTool.listeners.press(100, 200, new Event("mousedown"));
+            await sleep(80);
+            Tools.curTool.listeners.release(300, 400, new Event("mouseup"));
+            done();
+        })
+        .assert.elementPresent("path[d='M 100 200 L 100 200 C 100 200 300 400 300 400'][stroke='#123456']")
+        // Open new window, switch to it and navigate to URL
+        .openNewWindow()
+        .windowHandles(function (result) {
+            var handle = result.value[1];
+            browser.switchWindow(handle);
+        })
+        .url(URL)
+        // Assert element present on second window, including after refresh
+        .assert.elementPresent("path[d='M 100 200 L 100 200 C 100 200 300 400 300 400'][stroke='#123456']")
+        .refresh()
+        .assert.elementPresent("path[d='M 100 200 L 100 200 C 100 200 300 400 300 400'][stroke='#123456']")
+        // Close second window and switch back to first window.
+        .closeWindow()
+        .windowHandles(function (result) {
+            var handle = result.value[0];
+            browser.switchWindow(handle);
+        })
+        // Assert element is still present, then after refresh has been deleted.
+        .assert.elementPresent("path[d='M 100 200 L 100 200 C 100 200 300 400 300 400'][stroke='#123456']")
+        .refresh()
+        .assert.not.elementPresent("path[d='M 100 200 L 100 200 C 100 200 300 400 300 400'][stroke='#123456']")
+        .back()
+}
 
 function testBoardDelete(browser) {
-    var page = browser.url(SERVER + '/boards/delete-on-leave')
+    var page = browser.url(URL)
         .waitForElementVisible('.tool[title ~= Pencil]') // pencil
     page = testDeleteOnRefreshWithOneUser(page);
-
-    // test hideMenu
-    browser.url(SERVER + '/boards/delete-on-leave?hideMenu=true').waitForElementNotVisible('#menu');
-    browser.url(SERVER + '/boards/delete-on-leave?hideMenu=false').waitForElementVisible('#menu');
-
+    page = testDeleteOnRefreshWithTwoUsers(page);
     page.end();
 }
 
